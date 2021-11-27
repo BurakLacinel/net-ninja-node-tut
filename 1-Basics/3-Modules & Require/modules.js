227864100febdbe99dee0fdc5f3164ed95e23027
buraklacinel <buraklacinel@gmail.com>
2021-11-27 21:11:04 +0300

    export multiple objects

--- 1-Basics/3-Modules & Require/modules.js
+++ 1-Basics/3-Modules & Require/modules.js
@@ -5,3 +5,12 @@ const xyz = require("./people");
 
 //// "require" ile ulaştığımız dosyanın, içeriğine ulaşamayız.
 console.log(people); // Error "prople is not defined"
+
+// "people"a buradan ulaşabilmek istiyorsak, people objectini bulunduğu dosyada "export" etmemiz gerekir. Bu dosya da ise, "require"ı atadığımız değişken üzerinden ulaşabiliriz. (aynı isim de olaiblir ama zorunlu değil)
+console.log(xyz);
+
+console.log(xyz.people);
+console.log(xyz.ages);
+// // yukarıdaki gibi "dot" notation ile uğraşmamak için "object destructuring" yapabiliriz.
+// const { people, ages } = require("./people");
+// console.log(people, ages);
