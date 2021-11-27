701ea53519f8bc3e0f0ff4751af74d97f1223613
buraklacinel <buraklacinel@gmail.com>
2021-11-27 23:07:32 +0300

    fs directories

--- 1-Basics/4-File System/files.js
+++ 1-Basics/4-File System/files.js
@@ -16,11 +16,31 @@ const fs = require("fs");
 
 ////// WRITING FILES
 //// 1. parametre; relative path, 2.parametre; yazacağımız text, 3.parametre; işlem tamamlandığında çalışacak olan "callback function".
-fs.writeFile("./docs/blog1.txt", "hello, world", () => {
-  console.log("file was written");
-});
+// fs.writeFile("./docs/blog1.txt", "hello, world", () => {
+//   console.log("file was written");
+// });
 
-// Aynı işlemi var olmayan bir dosyaya uygularsak, dosya Node tarafından oluşturulur.
-fs.writeFile("./docs/blog2.txt", "hello, world", () => {
-  console.log("file was written");
-});
+// // Aynı işlemi var olmayan bir dosyaya uygularsak, dosya Node tarafından oluşturulur.
+// fs.writeFile("./docs/blog2.txt", "hello, world", () => {
+//   console.log("file was written");
+// });
+
+//// DIRECTORIES
+// Yeni bir klasör (folder) oluşturmak için "mkdir()" kullanırız.
+// Var olan klasörü silmek için, "rmdir()" kullanırız.
+// Var olup olmadığını "existsSync()" ile öğrenebiliriz.
+if (!fs.existsSync("./img")) {
+  fs.mkdir("./img", (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("folder created");
+  });
+} else {
+  fs.rmdir("./img", (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("folder deleted");
+  });
+}
