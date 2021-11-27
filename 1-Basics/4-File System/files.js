b2b28cc33fea4edbe634f3c5b345dbcf6c33844c
buraklacinel <buraklacinel@gmail.com>
2021-11-27 22:43:03 +0300

    fs writeFile

--- 1-Basics/4-File System/files.js
+++ 1-Basics/4-File System/files.js
@@ -2,14 +2,25 @@
 
 const fs = require("fs");
 
-// READING FILES
-fs.readFile("./docs/blog1.txt", (err, data) => {
-  if (err) {
-    console.log(err);
-  }
-  // console.log(data); // "buffer" döndürür.
-  console.log(data.toString());
+// // READING FILES
+// fs.readFile("./docs/blog1.txt", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   // console.log(data); // "buffer" döndürür.
+//   console.log(data.toString());
+// });
+
+// // "readFile()" asenkron çalışır.
+// console.log("something after readFile()");
+
+////// WRITING FILES
+//// 1. parametre; relative path, 2.parametre; yazacağımız text, 3.parametre; işlem tamamlandığında çalışacak olan "callback function".
+fs.writeFile("./docs/blog1.txt", "hello, world", () => {
+  console.log("file was written");
 });
 
-// "readFile()" asenkron çalışır.
-console.log("something after readFile()");
+// Aynı işlemi var olmayan bir dosyaya uygularsak, dosya Node tarafından oluşturulur.
+fs.writeFile("./docs/blog2.txt", "hello, world", () => {
+  console.log("file was written");
+});
